1a2a4ab1406d9e0b54983dfdf1d8002abc519555
gigi <gigi.shalamberidze2022@gmail.com>
2023-10-03 20:14:56 +0400

    fixed navbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="relative w-full md:h-screen lg:p-3">
-    <div class="relative w-full h-full flex flex-col-reverse md:flex-col lg:flex-row gap-3">
+    <div class="relative w-full h-full flex flex-col-reverse md:flex-col lg:flex-row gap-3 pb-24 md:pb-0">
       <Sidebar :isActive="sidebarActive" @closeSidebar="closeSidebar" />
       <div class="w-full bg-white nav h-auto rounded overflow-auto no-scrollbar px-5 md:px-10 lg:px-20">
         <Header :isActive="sidebarActive" @toggleSidebar="toggleSidebar" />
